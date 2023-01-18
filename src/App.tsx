eb99bb75a68f495c79b902317ef83564bc0ff712
Hafiz Huseynov <hafiz.huseynov@wevoteam.com>
2023-01-18 15:56:50 +0400

    svg support : component and url

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,17 @@
+import ManImage from "./assets/man.jpg";
+import HomeIcon from "./assets/icons/home.svg";
+import HomeIconUrl from "./assets/icons/home.svg?url";
+
 const App = () => {
-  return <h1>React template</h1>;
+  return (
+    <div>
+      <h1>React template</h1>
+      <div>
+        <img src={HomeIconUrl} />
+        <HomeIcon width={20} height={20} />
+      </div>
+    </div>
+  );
 };
 
 export default App;
